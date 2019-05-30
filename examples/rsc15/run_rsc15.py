0c0f9b02e07adafa2b8447e3462cd1c31ccc2a9c
Fausztin Asztrik Virág <fuszti@gmail.com>
2019-05-30 21:43:02 +0200

    Added the numpy file reading

--- examples/rsc15/run_rsc15.py
+++ examples/rsc15/run_rsc15.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     parser.add_argument('--batch_size', action="store", dest='batch_size', default=32, type=int)
     parser.add_argument('--learning_rate', action="store", dest='learning_rate', default=0.1, type=float)
     parser.add_argument('--n_sample', action="store", dest='n_sample', default=2048, type=int)
-    #parser.add_argument('--batch_size', action="store", dest='hidden', default=32, type=int)
+    parser.add_argument('--embedding_file', action="store", dest='embedding_file', default=None)
     parser.add_argument('--adapt', action="store", dest='adapt', default=None)
     args=parser.parse_args()
 
@@ -57,6 +57,15 @@ if __name__ == '__main__':
 
     #Cross-entropy (R@20 = 0.7180, M@20 = 0.3087)
     #gru = gru4rec.GRU4Rec(loss='cross-entropy', final_act='softmax', hidden_act='tanh', layers=[100], adapt='adagrad', n_epochs=10, batch_size=32, dropout_p_embed=0, dropout_p_hidden=0.3, learning_rate=0.1, momentum=0.7, n_sample=2048, sample_alpha=0, bpreg=0, constrained_embedding=False)
+    if args.embedding_file is None:
+        emb = 0
+        E = np.array([0])
+    else:
+        emb = -1
+        f=open(args.embedding_file, 'r')
+        E = np.load(f)
+        f.close()
+
     gru = gru4rec.GRU4Rec(
         loss='cross-entropy', 
         final_act='softmax', 
@@ -72,33 +81,36 @@ if __name__ == '__main__':
         n_sample=args.n_sample, 
         sample_alpha=0, 
         bpreg=0, 
-        constrained_embedding=False)
+        constrained_embedding=False,
+        embedding=emb,
+        embedding_matrix=E
+        )
     gru.fit(data)
     res = evaluation.evaluate_gpu(gru, valid)
     print('Recall@20: {}'.format(res[0]))
     print('MRR@20: {}'.format(res[1]))
 
     #TOP1
-    gru = gru4rec.GRU4Rec(
-        loss='top1', 
-        final_act='softmax', 
-        hidden_act='tanh', 
-        layers=[args.hidden], 
-        adapt='adagrad', 
-        n_epochs=args.epoch, 
-        batch_size=args.batch_size, 
-        dropout_p_embed=0, 
-        dropout_p_hidden=0.3, 
-        learning_rate=args.learning_rate, 
-        momentum=0.7, 
-        n_sample=args.n_sample, 
-        sample_alpha=0, 
-        bpreg=0, 
-        constrained_embedding=False)
-    gru.fit(data)
-    res = evaluation.evaluate_gpu(gru, valid)
-    print('Recall@20: {}'.format(res[0]))
-    print('MRR@20: {}'.format(res[1]))
+    # gru = gru4rec.GRU4Rec(
+    #     loss='top1', 
+    #     final_act='softmax', 
+    #     hidden_act='tanh', 
+    #     layers=[args.hidden], 
+    #     adapt='adagrad', 
+    #     n_epochs=args.epoch, 
+    #     batch_size=args.batch_size, 
+    #     dropout_p_embed=0, 
+    #     dropout_p_hidden=0.3, 
+    #     learning_rate=args.learning_rate, 
+    #     momentum=0.7, 
+    #     n_sample=args.n_sample, 
+    #     sample_alpha=0, 
+    #     bpreg=0, 
+    #     constrained_embedding=False)
+    # gru.fit(data)
+    # res = evaluation.evaluate_gpu(gru, valid)
+    # print('Recall@20: {}'.format(res[0]))
+    # print('MRR@20: {}'.format(res[1]))
     
     #OUTDATED!!!
     #Reproducing results from the original paperr"Session-based Recommendations with Recurrent Neural Networks" on RSC15 (http://arxiv.org/abs/1511.06939)
