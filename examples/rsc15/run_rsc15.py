9921b18828facd8f2808e82e4d4d6b73cb53e6b3
Fausztin Asztrik Virág <fuszti@gmail.com>
2019-05-30 14:37:49 +0200

    Add argparser for parameters in running

--- examples/rsc15/run_rsc15.py
+++ examples/rsc15/run_rsc15.py
@@ -26,6 +26,13 @@ if __name__ == '__main__':
                         dest="train")
     parser.add_argument('--test', action="store",
                         dest="test")
+    parser.add_argument('--hidden', action="store", dest='hidden', default=100, type=int)
+    parser.add_argument('--epoch', action="store", dest='hidden', default=1, type=int)
+    parser.add_argument('--batch_size', action="store", dest='hidden', default=32, type=int)
+    parser.add_argument('--learning_rate', action="store", dest='hidden', default=0.1, type=float)
+    parser.add_argument('--n_sample', action="store", dest='hidden', default=2048, type=int)
+    #parser.add_argument('--batch_size', action="store", dest='hidden', default=32, type=int)
+    parser.add_argument('--adapt', action="store", dest='adapt', default=None)
     args=parser.parse_args()
 
     PATH_TO_TRAIN=args.train
@@ -35,21 +42,59 @@ if __name__ == '__main__':
     
     #State-of-the-art results on RSC15 from "Recurrent Neural Networks with Top-k Gains for Session-based Recommendations" on RSC15 (http://arxiv.org/abs/1706.03847)
     #BPR-max, no embedding (R@20 = 0.7197, M@20 = 0.3157)
-    gru = gru4rec.GRU4Rec(loss='bpr-max', final_act='elu-0.5', hidden_act='tanh', layers=[100], adapt='adagrad', n_epochs=10, batch_size=32, dropout_p_embed=0, dropout_p_hidden=0, learning_rate=0.2, momentum=0.3, n_sample=2048, sample_alpha=0, bpreg=1, constrained_embedding=False)
-    gru.fit(data)
-    res = evaluation.evaluate_gpu(gru, valid)
-    print('Recall@20: {}'.format(res[0]))
-    print('MRR@20: {}'.format(res[1]))
+    #gru = gru4rec.GRU4Rec(loss='bpr-max', final_act='elu-0.5', hidden_act='tanh', layers=[100], adapt='adagrad', n_epochs=10, batch_size=32, dropout_p_embed=0, dropout_p_hidden=0, learning_rate=0.2, momentum=0.3, n_sample=2048, sample_alpha=0, bpreg=1, constrained_embedding=False)
+    #gru.fit(data)
+    #res = evaluation.evaluate_gpu(gru, valid)
+    #print('Recall@20: {}'.format(res[0]))
+    #print('MRR@20: {}'.format(res[1]))
 
     #BPR-max, constrained embedding (R@20 = 0.7261, M@20 = 0.3124)
-    gru = gru4rec.GRU4Rec(loss='bpr-max', final_act='elu-0.5', hidden_act='tanh', layers=[100], adapt='adagrad', n_epochs=10, batch_size=32, dropout_p_embed=0, dropout_p_hidden=0, learning_rate=0.2, momentum=0.1, n_sample=2048, sample_alpha=0, bpreg=0.5, constrained_embedding=True)
+    #gru = gru4rec.GRU4Rec(loss='bpr-max', final_act='elu-0.5', hidden_act='tanh', layers=[100], adapt='adagrad', n_epochs=10, batch_size=32, dropout_p_embed=0, dropout_p_hidden=0, learning_rate=0.2, momentum=0.1, n_sample=2048, sample_alpha=0, bpreg=0.5, constrained_embedding=True)
+    #gru.fit(data)
+    #res = evaluation.evaluate_gpu(gru, valid)
+    #print('Recall@20: {}'.format(res[0]))
+    #print('MRR@20: {}'.format(res[1]))
+
+    #Cross-entropy (R@20 = 0.7180, M@20 = 0.3087)
+    #gru = gru4rec.GRU4Rec(loss='cross-entropy', final_act='softmax', hidden_act='tanh', layers=[100], adapt='adagrad', n_epochs=10, batch_size=32, dropout_p_embed=0, dropout_p_hidden=0.3, learning_rate=0.1, momentum=0.7, n_sample=2048, sample_alpha=0, bpreg=0, constrained_embedding=False)
+    gru = gru4rec.GRU4Rec(
+        loss='cross-entropy', 
+        final_act='softmax', 
+        hidden_act='tanh', 
+        layers=[args.hidden], 
+        adapt='adagrad', 
+        n_epochs=args.epoch, 
+        batch_size=args.batch_size, 
+        dropout_p_embed=0, 
+        dropout_p_hidden=0.3, 
+        learning_rate=args.learning_rate, 
+        momentum=0.7, 
+        n_sample=args.n_sample, 
+        sample_alpha=0, 
+        bpreg=0, 
+        constrained_embedding=False)
     gru.fit(data)
     res = evaluation.evaluate_gpu(gru, valid)
     print('Recall@20: {}'.format(res[0]))
     print('MRR@20: {}'.format(res[1]))
 
-    #Cross-entropy (R@20 = 0.7180, M@20 = 0.3087)
-    gru = gru4rec.GRU4Rec(loss='cross-entropy', final_act='softmax', hidden_act='tanh', layers=[100], adapt='adagrad', n_epochs=10, batch_size=32, dropout_p_embed=0, dropout_p_hidden=0.3, learning_rate=0.1, momentum=0.7, n_sample=2048, sample_alpha=0, bpreg=0, constrained_embedding=False)
+    #TOP1
+    gru = gru4rec.GRU4Rec(
+        loss='top1', 
+        final_act='softmax', 
+        hidden_act='tanh', 
+        layers=[args.hidden], 
+        adapt='adagrad', 
+        n_epochs=args.epoch, 
+        batch_size=args.batch_size, 
+        dropout_p_embed=0, 
+        dropout_p_hidden=0.3, 
+        learning_rate=args.learning_rate, 
+        momentum=0.7, 
+        n_sample=args.n_sample, 
+        sample_alpha=0, 
+        bpreg=0, 
+        constrained_embedding=False)
     gru.fit(data)
     res = evaluation.evaluate_gpu(gru, valid)
     print('Recall@20: {}'.format(res[0]))
